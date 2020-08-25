7b7bdcf3b0f0bee9d9b2ba8fcf71532122401151
Andrew Konoff <andrew.konoff@gmail.com>
2020-08-25 16:32:31 -0400

    added tutorial template but also fixed URLs

--- hubspot-components/deal-fishing-tutorial.module/module.js
+++ hubspot-components/deal-fishing-tutorial.module/module.js
@@ -14,13 +14,13 @@ var game = new Phaser.Game(config);
 function preload ()
 {
 	// this gets called once at the very beginning when `game` is instantiated
-  this.load.image('ground', 'https://f.hubspotusercontent20.net/hubfs/1234567/assets/atlas.png');
-	this.load.spritesheet('pond', 'https://f.hubspotusercontent20.net/hubfs/1234567/assets/pond.png', { frameWidth: 54, frameHeight: 39});
-	this.load.spritesheet('tile', 'https://f.hubspotusercontent20.net/hubfs/1234567/assets/tile.png', { frameWidth: 16, frameHeight: 16});
+  this.load.image('ground', 'https://f.hubspotusercontent20.net/hubfs/8311725/assets/atlas.png');
+	this.load.spritesheet('pond', 'https://f.hubspotusercontent20.net/hubfs/8311725/assets/pond.png', { frameWidth: 54, frameHeight: 39});
+	this.load.spritesheet('tile', 'https://f.hubspotusercontent20.net/hubfs/8311725/assets/tile.png', { frameWidth: 16, frameHeight: 16});
 
-	this.load.spritesheet('fishA', 'https://f.hubspotusercontent20.net/hubfs/1234567/assets/FishA.png', { frameWidth: 16, frameHeight: 16});
+	this.load.spritesheet('fishA', 'https://f.hubspotusercontent20.net/hubfs/8311725/assets/FishA.png', { frameWidth: 16, frameHeight: 16});
 
-	this.load.spritesheet('player', 'https://f.hubspotusercontent20.net/hubfs/1234567/assets/hero.png', { frameWidth: 16, frameHeight: 24});
+	this.load.spritesheet('player', 'https://f.hubspotusercontent20.net/hubfs/8311725/assets/hero.png', { frameWidth: 16, frameHeight: 24});
 }
 
 function create ()
@@ -28,7 +28,7 @@ function create ()
 	// this gets called once, after `preload` is finished
 	// anything loaded in `preload` is guaranteed to be accessible here
   this.add.image(400, 300, 'ground').setScale(2);
-  
+
 }
 
 function update ()
