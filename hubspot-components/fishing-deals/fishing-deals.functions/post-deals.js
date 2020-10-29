d076b518bb82069df33cd43533bdd4d0314fe1ed
Andrew Konoff <andrew.konoff@gmail.com>
2020-10-29 17:02:56 -0400

    Updated function to use api-client

--- hubspot-components/fishing-deals/fishing-deals.functions/post-deals.js
+++ hubspot-components/fishing-deals/fishing-deals.functions/post-deals.js
@@ -1,4 +1,4 @@
-var request = require("request");
+const hubspot = require('@hubspot/api-client');
 
 var fishNames = ["Nemo",  "Bubbles",  "Jack",  "Captain",  "Finley",  "Blue",  "Moby",  "Bubba",  "Squirt",  "Shadow",  "Goldie",  "Dory",  "Ariel",  "Angel",  "Minnie",  "Jewel",  "Nessie",  "Penny",  "Crystal",  "Coral"]
 var colors = ["White", "Yellow", "Blue", "Red", "Green", "Black", "Brown", "Azure", "Ivory", "Teal", "Silver", "Purple", "Navy", "PeaGreen", "Gray", "Orange", "Maroon", "Charcoal", "Aquamarine", "Coral", "Fuchsia", "Wheat", "Lime", "Crimson", "Khaki", "HotPink", "Magenta", "Olden", "Plum", "Olive", "Cyan"]
@@ -7,36 +7,34 @@ exports.main = ({ accountId, body, params }, sendResponse) => {
   console.log('Your HubSpot account ID: %i', accountId);
   console.log('Your fish size: ' + body.fish_size);
 
-  var firstName = fishNames[Math.floor(Math.random() * fishNames.length)];
-  var lastName = ("Mc" + colors[Math.floor(Math.random() * colors.length)]);
+  const firstName = fishNames[Math.floor(Math.random() * fishNames.length)];
+  const lastName = ("Mc" + colors[Math.floor(Math.random() * colors.length)]);
 
-  var options = {
-    method: 'POST',
-    url: 'https://api.hubapi.com/crm/v3/objects/contacts',
-    qs: {hapikey: process.env.hubapikey},
-    headers: {accept: 'application/json', 'content-type': 'application/json'},
-    body: {
-      properties: {
-        firstname: firstName,
-        lastname: lastName,
-        annualrevenue: body.fish_size
-      }
-    },
-    json: true
-  };
 
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+  const contactObj = {
+    properties: {
+      firstname: firstName,
+      lastname: lastName,
+      annualrevenue: body.fish_size
+    }
+  }
 
-    console.log("The " + firstName + " " + lastName + " contact was successfully created");
+  const hubspotClient = new hubspot.Client({ apiKey: process.env.hubapikey});
+  hubspotClient.crm.contacts.basicApi.create(contactObj)
+    .then(result => {
+      console.log(result);
+      
+      sendResponse({
+        statusCode: 201,
+        body: {
+          fish_name: (firstName + ' ' + lastName),
+          score: parseInt(result.body.properties.annualrevenue),
+          message: ('Your fish is named ' + firstName + ' ' + lastName)
+        },
+      });
 
-    sendResponse({
-      statusCode: 201,
-      body: {
-        fish_name: (firstName + ' ' + lastName),
-        score: parseInt(body.properties.annualrevenue),
-        message: ('Your fish is named ' + firstName + ' ' + lastName)
-      },
-    });
-  });
+    })
+    .catch(err => {
+      throw new Error(err);
+    })
 };
